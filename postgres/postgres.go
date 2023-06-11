f95d1845a4fabb064a69226ef7efa1ab03a7edfd
Allen Yao <allen@Allens-MacBook-Pro.local>
2023-06-10 22:38:44 -0700

    migrate and close for db

--- postgres/postgres.go
+++ postgres/postgres.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"time"
 
+	"github.com/xueyiyao/safekeep/domain"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
@@ -29,6 +30,36 @@ func (db *DB) Open() (err error) {
 
 	if db.DB, err = gorm.Open(postgres.Open(db.DSN), &gorm.Config{}); err != nil {
 		return err
+	}
+
+	if err := db.migrate(); err != nil {
+		return fmt.Errorf("migrate: %w", err)
+	}
+
+	return nil
+}
+
+func (db *DB) migrate() (err error) {
+	if err := db.DB.AutoMigrate(&domain.User{}); err != nil {
+		return err
+	}
+	if err := db.DB.AutoMigrate(&domain.Container{}); err != nil {
+		return err
+	}
+	if err := db.DB.AutoMigrate(&domain.Item{}); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (db *DB) Close() error {
+	if db.DB != nil {
+		db, err := db.DB.DB()
+		if err != nil {
+			return err
+		}
+		return db.Close()
 	}
 
 	return nil
