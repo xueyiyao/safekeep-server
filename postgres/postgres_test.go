a5fb2059da6c38de422c16770db1747dfd9425d8
Allen Yao <allen@Allens-MacBook-Pro.local>
2023-06-10 23:23:51 -0700

    add postgres test file

---
+++ postgres/postgres_test.go
@@ -0,0 +1,50 @@
+package postgres_test
+
+import (
+	"fmt"
+	"os"
+	"testing"
+
+	"github.com/joho/godotenv"
+	"github.com/xueyiyao/safekeep/internal/path"
+	"github.com/xueyiyao/safekeep/postgres"
+)
+
+func TestDB(t *testing.T) {
+	db := MustOpenDB(t)
+	MustCloseDB(t, db)
+}
+
+func MustOpenDB(tb testing.TB) *postgres.DB {
+	tb.Helper()
+
+	if err := godotenv.Load(path.Root + "/.env"); err != nil {
+		tb.Fatal(err)
+	}
+
+	dsn := getDSN()
+	db := postgres.NewDB(dsn)
+	if err := db.Open(); err != nil {
+		tb.Fatal(err)
+	}
+
+	return db
+}
+
+func MustCloseDB(tb testing.TB, db *postgres.DB) {
+	tb.Helper()
+
+	if err := db.Close(); err != nil {
+		tb.Fatal(err)
+	}
+}
+
+func getDSN() string {
+	host := os.Getenv("TEST_DB_HOST")
+	port := os.Getenv("TEST_DB_PORT")
+	user := os.Getenv("TEST_DB_USER")
+	password := os.Getenv("TEST_DB_PASSWORD")
+	dbname := os.Getenv("TEST_DB_NAME")
+
+	return fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
+}
